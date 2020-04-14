aac6ddae114780b06fc903402345d491044c8b1c
shinkaza <61479763+shinkaza@users.noreply.github.com>
2020-04-14 00:43:12 -0500

    Add files via upload

---
+++ sentencias.sql
@@ -0,0 +1,5 @@
+use uniminutodb;
+select * from fakenames where edad>18 and edad<32;
+select * from fakenames where sexo ="male";
+select * from fakenames where sexo ="female" and edad>25;
+select * from fakenames where apellido="toledo";
